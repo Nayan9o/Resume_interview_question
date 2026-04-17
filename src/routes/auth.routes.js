90f2169300121592f8bf4f9cf22b63db018160d8
Nayan9o <nayanmeshram9o9@gmail.com>
2026-04-17 22:57:35 +0530

    add logout route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,9 +3,14 @@ const authController = require('../controllers/auth.controller')
 
 const authRoutes = Router()
 
-
+//register route
 authRoutes.post("/register",authController.registerUserController)
 
+
+//login route
 authRoutes.post("/login",authController.loginUserController)
 
+
+//logout route
+authRoutes.get("/logout",authController.logoutUserController)
 module.exports = authRoutes
