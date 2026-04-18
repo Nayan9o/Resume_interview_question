c001f861986735dcb1048f7f905ffdddc1520ed3
Nayan9o <nayanmeshram9o9@gmail.com>
2026-04-18 21:39:57 +0530

    create frontend

---
+++ Frontend/src/app.routes.jsx
@@ -0,0 +1,5 @@
+import {createBrowserRouter} from 'react-router'
+import Login from './features/auth/pages/Login'
+import Register from './features/auth/pages/register'
+
+export const router = createBrowserRouter([])
