1569f963fa4e547f5b50051a197e01948f723f20
Nayan9o <nayanmeshram9o9@gmail.com>
2026-04-19 00:29:32 +0530

    create login api:

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,11 +1,28 @@
 import axios from 'axios'
 
+//register api
 export async function register({username, email, password}){
 
   try{
 
   const responce = await  axios.post('http://localhost:3000/api/auth/register',{
       username, email, password
+    },{
+      withCredentials:true
+    })
+
+    return responce.data
+  }catch(err){
+    console.log(err)
+  }
+}
+
+//login api
+export async function  login ({email,password}){
+
+  try{
+    const responce  =  await axios.post('http://localhost:3000/api/auth/login',{
+      email, password
     },{
       withCredentials:true
     })
