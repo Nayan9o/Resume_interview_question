ef95f7a6f02404dc4606f778051435de56daac8a
Nayan9o <nayanmeshram9o9@gmail.com>
2026-04-18 22:12:11 +0530

    create login from

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      
-    </div>
+    <main>
+      <div className="form-contener">
+        <h1>Login</h1>
+        <form action="">
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" placeholder='Enter email address' id='email' name='email' />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" placeholder='Enter password ' id='password' name='password' />
+          </div>
+          <button className='button primary-button'> Login </button>
+        </form>
+      </div>
+    </main>
   )
 }
 
