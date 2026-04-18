4f14909b95994dcc045e403888d3c70c3b67e765
Nayan9o <nayanmeshram9o9@gmail.com>
2026-04-19 00:59:53 +0530

    handleLogin create

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,56 +1,56 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-  baseURL:'http://localhost:3000',
-  withCredentials:true
-})
+  baseURL: "http://localhost:3000",
+  withCredentials: true,
+});
 
 //register api
-export async function register({username, email, password}){
+export async function register({ username, email, password }) {
+  try {
+    const responce = await api.post("/api/auth/register", {
+      username,
+      email,
+      password,
+    });
 
-  try{
-
-  const responce = await  api.post('/api/auth/register',{
-      username, email, password
-    })
-
-    return responce.data
-  }catch(err){
-    console.log(err)
+    return responce.data;
+  } catch (err) {
+    console.log(err);
   }
 }
 
 //login api
-export async function  login ({email,password}){
+export async function login({ email, password }) {
+  try {
+    const responce = await api.post("/api/auth/login", {
+      email,
+      password,
+    });
 
-  try{
-    const responce  =  await api.post('/api/auth/login',{
-      email, password
-    })
-
-    return responce.data
-  }catch(err){
-    console.log(err)
+    return responce.data;
+  } catch (err) {
+    console.log(err);
   }
 }
 
 //logout api
-export async function logout(){
-  try{
-    const responce = await api.get('/api/auth/logout')
-    return responce.data
-  }catch(err){
-    console.log(err)
+export async function logout() {
+  try {
+    const responce = await api.get("/api/auth/logout");
+    return responce.data;
+  } catch (err) {
+    console.log(err);
   }
 }
 
 //get me
-export async function getMe(){
-  try{
-    const responce = await api.get('/api/auth/get-me')
+export async function getMe() {
+  try {
+    const responce = await api.get("/api/auth/get-me");
 
-    return responce.data
-  }catch(err){
-    console.log(err)
+    return responce.data;
+  } catch (err) {
+    console.log(err);
   }
 }
