8d8f3553b4d563586725b48f55a4e25cddffc746
Nayan9o <nayanmeshram9o9@gmail.com>
2026-04-19 00:40:42 +0530

    create get me api

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,14 +1,17 @@
 import axios from 'axios'
 
+const api = axios.create({
+  baseURL:'http://localhost:3000',
+  withCredentials:true
+})
+
 //register api
 export async function register({username, email, password}){
 
   try{
 
-  const responce = await  axios.post('http://localhost:3000/api/auth/register',{
+  const responce = await  api.post('/api/auth/register',{
       username, email, password
-    },{
-      withCredentials:true
     })
 
     return responce.data
@@ -21,10 +24,8 @@ export async function register({username, email, password}){
 export async function  login ({email,password}){
 
   try{
-    const responce  =  await axios.post('http://localhost:3000/api/auth/login',{
+    const responce  =  await api.post('/api/auth/login',{
       email, password
-    },{
-      withCredentials:true
     })
 
     return responce.data
@@ -36,9 +37,18 @@ export async function  login ({email,password}){
 //logout api
 export async function logout(){
   try{
-    const responce = await axios.get('http://localhost:3000/api/auth/logout',{
-      withCredentials:true
-    })
+    const responce = await api.get('/api/auth/logout')
+    return responce.data
+  }catch(err){
+    console.log(err)
+  }
+}
+
+//get me
+export async function getMe(){
+  try{
+    const responce = await api.get('/api/auth/get-me')
+
     return responce.data
   }catch(err){
     console.log(err)
