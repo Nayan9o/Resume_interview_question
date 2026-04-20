a4b069be3be9bd4b6c38ab29c9fb6b3a037f4266
Nayan9o <nayanmeshram9o9@gmail.com>
2026-04-20 21:41:44 +0530

    Handle cors error

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 const authRoutes = require('./routes/auth.routes')// require routes
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 app.use(express.json())
 app.use(cookieParser())
-
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}))
 
 //usea all the routes
 app.use("/api/auth",authRoutes)
