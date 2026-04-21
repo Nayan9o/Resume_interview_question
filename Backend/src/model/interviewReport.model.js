c0f2cb45e5ba2995694acdb747c25888e54fffaf
Nayan9o <nayanmeshram9o9@gmail.com>
2026-04-21 23:56:44 +0530

    Comment what type of Schema

--- Backend/src/model/interviewReport.model.js
+++ Backend/src/model/interviewReport.model.js
@@ -1,1 +1,22 @@
-c
+const mongoose = require('mongoose')
+
+/**
+ * - job description schema
+ * -resume text
+ * - self description
+ * -Match score : number
+ * -Technical questins :
+ * [{
+ * quesiton:''&interion:' '  & answer: ''}]
+ * -Behavioral questions: []
+ * Skill gaps :  [{
+ *          skills : "",
+ *          severity : {
+ *            type: String,
+ *          enum: [low,medium,high]}}]
+ * -preparation plan : [{
+ *      day: number,
+ *    foucs: string,
+ *    tasks: [string]}]
+ * 
+ */
